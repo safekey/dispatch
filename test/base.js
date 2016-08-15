3c0636ba78c03b5acd3f9791980325508a90ec1c
Nicholas White <nicholas@nicholasjwhite.net>
2016-08-15 17:39:03 +0000

    Add test setup for initial deploy

---
+++ test/base.js
@@ -0,0 +1,8 @@
+var expect    = require("chai").expect;
+var request = require("request");
+
+describe("Base http server", function(){
+    it("Adds numbers correctly", function() {
+        expect(1+1).to.equal(2);
+    });
+});
